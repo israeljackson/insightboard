f0b88fed4d8d246d158361ac580feba6f9091c68
Jackson Israel <jacksonisrael707@gmail.com>
2026-03-04 15:08:22 +0100

    fix: correct ProductsDetailsModal import casing for production build

--- src/components/ProductList.jsx
+++ src/components/ProductList.jsx
@@ -1,5 +1,5 @@
 import { useState } from "react";
-import ProductsDetailsModal from "./productsDetailsModal";
+import ProductsDetailsModal from "./ProductsDetailsModal";
 import ProductCard from "../components/ProductCard";
 
 function ProductList({
